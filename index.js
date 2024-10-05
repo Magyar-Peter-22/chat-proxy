af058595c17dce16e966e0019e53b5c718de8395
Magyar-Peter-22 <81881238+Magyar-Peter-22@users.noreply.github.com>
2024-10-05 19:22:16 +0200

    start

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import httpProxy from "http-proxy";
+import env from "dotenv";
+import cors from "cors";
+env.config();
+
+const app = express();
+const proxy = httpProxy.createProxyServer();
+app.use(cors());
+
+function rewrite(options) {
+    return (req, res) => {
+        console.log(`rewriting "${req.url}" to "${options.target}"`);
+        proxy.web(req, res, options)
+    }
+}
+
+console.log(process.env.API_URL)
+app.use('/api', rewrite({
+    target: process.env.API_URL,
+    ws: true
+}));
+app.use('/', rewrite({
+    target: process.env.CLIENT_URL
+}));
+
+proxy.on("error", (err) => {
+    console.log("\nproxy error:\n");
+    console.log(err);
+})
+
+const port = process.env.PORT || 3002;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
